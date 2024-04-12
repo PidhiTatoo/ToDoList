01d75ed87b5f1fd9715e96236746f423ca602a94
Abdullah <abdullah.goteborg@gmail.com>
2024-04-12 11:40:41 +0200

    trying to commit

--- to-do-list.py
+++ to-do-list.py
@@ -1,5 +1,11 @@
+# todo_list.py
+
+#This code creates a list that we can use to create a to do list like mine.
 tasks = []
 
+#Here I use def to define the functions shown below. I also use if and else so I can have 3 functions at the same time.
+#Also a new thing, to me atleast is the enumerate. It's a way to give a number to objects, it starts with 0 and goes upwards from there.
+#If you want to you can change it so it starts with 1 instead but I find it uneccessary as this code MAY be scrapped anyway..
 def add_task(task):
     tasks.append(task)
     print("Task added successfully!")
@@ -12,22 +18,23 @@ def view_tasks():
         for index, task in enumerate(tasks, start=1):
             print(f"{index}. {task}")
 
-def remove_task(task_name):
-    removed = False
-    for task in tasks:
-        if task.lower() == task_name.lower():
-            tasks.remove(task)
-            print(f"Task '{task}' removed successfully!")
-            removed = True
-            break
-    if not removed:
-        print("Task not found!")
+def remove_task(task_index):
+    try:
+        del tasks[task_index - 1]
+        print("Task removed successfully!")
+    except IndexError:
+        print("Invalid task index!")
 
+#As you can see this is just to print the thing you wanna do, nothing special.
 print("Select operation:")
 print("1. Add Task")
 print("2. View Tasks")
 print("3. Remove Task")
 
+#This part of the code allows you to choose if you want to add daily tasks, see them and delete them.
+#Choice 1 is to add, choice 2 is to see the status and choice 3 is to delete, however to delete you have to press a number instead of its name
+#depending on when you added the task. so if you have 3 tasks (Cheese, dolphin, horse) cheese would be 0, dolphin 1 and horse 2.
+#It has to do with the enumerate explained earlier. 
 while True:
     choice = input("Enter choice (1/2/3): ")
 
@@ -37,7 +44,7 @@ while True:
     elif choice == '2':
         view_tasks()
     elif choice == '3':
-        task_name = input("Enter task name to remove: ")
-        remove_task(task_name)
+        task_index = int(input("Enter task index to remove: "))
+        remove_task(task_index)
     else:
         print("Invalid input")
