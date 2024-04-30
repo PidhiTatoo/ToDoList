fb382348edad45d74badefdd4f944e781fa7927b
Abdullah <abdullah.goteborg@gmail.com>
2024-04-30 16:13:03 +0200

    FIXED the comments, hopefully they look good. If not tell me so I can fix it:)

--- to-do-list.py
+++ to-do-list.py
@@ -1,43 +1,59 @@
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import simpledialog #Simpledialog allows us to have this ui we are using right now. It looks a bit old but I-
+#-couldn't get the themes to work.
 
-# Function to add a new task after prompting user input
+
+# This function lets you add a new task to your list. When you run it,
+# it asks you what task you want to add. If you tell it something, it will remember that task and show it on the screen.
 def add_task():
     task = simpledialog.askstring("Input", "Enter task:")
-    if task:  # Ensures only non-empty tasks are added
+    if task:  # Make sure the task isn't empty
         tasks.append(task)
-        update_tasks()  # Refresh the listbox view with the new task list
+        update_tasks()  # Now, let's update the screen to show your new task, without this it wont even appear.
 
-# Function to remove a selected task from the list
+# This function helps you remove a task you don't want anymore. It checks if you
+# have chosen a task to remove, and if you have, it takes it off your list and updates the screen to show the change.
 def remove_task():
-    if lstbox_tasks.curselection():  # Checks if there is any item selected
+    if lstbox_tasks.curselection():  # Checks if you've selected a task
         tasks.pop(lstbox_tasks.curselection()[0])
-        update_tasks()  # Refresh the listbox view after removal
+        update_tasks()  # Updates the screen to show the remaining tasks
 
-# Function to update the listbox display with current tasks
+# Here we update the list on the screen with all the tasks you've told it. First,
+# it clears the old list and then adds all the tasks back in so you can see everything you need to do!
 def update_tasks():
-    lstbox_tasks.delete(0, tk.END)  # Clears the current listbox contents
-    for task in tasks:  # Re-populates the listbox with current tasks
+    lstbox_tasks.delete(0, tk.END)  # Clear the list on the screen
+    for task in tasks:  # Add each task back to the list on the screen
         lstbox_tasks.insert(tk.END, task)
 
-# Set up the main window
+# Setting up the main window of our app where everything happens. It's like the stage
+# for our little task-show where we get to add and remove tasks!
 root = tk.Tk()
 root.title("ToDo List")
 
-# List to store tasks
+# Here's a list that will hold all your tasks. You can think of it like a magic
+# box that keeps all the tasks you need to remember.
 tasks = []
 
-# Creating the listbox widget to display tasks
+# This is a special box where we show all the tasks you need to do. It's like
+# a display board that lets you see everything clearly.
 lstbox_tasks = tk.Listbox(root, height=10, width=50)
-lstbox_tasks.pack(pady=10)  # Add some padding around the listbox
+lstbox_tasks.pack(pady=10)  # We add some space around the box so it looks nice.
 
-# Creating and setting up the button to add tasks
+# This is a button that adds a new task. When you click it, it starts the process
+# of adding a task to your list.
 btn_add_task = tk.Button(root, text="Add Task", command=add_task)
-btn_add_task.pack(fill=tk.X)  # Make the button expand to fill the X direction
+btn_add_task.pack(fill=tk.X)  # The button grows to fill the space it's in, so it's easy to click.
 
-# Creating and setting up the button to remove tasks
+# This button lets you remove a task from your list. It's super helpful for
+# keeping your list neat and tidy.
 btn_remove_task = tk.Button(root, text="Remove Task", command=remove_task)
-btn_remove_task.pack(fill=tk.X)  # Make the button expand to fill the X direction
+btn_remove_task.pack(fill=tk.X)  # This button also grows to fill its space.
 
-# Starts the event loop (makes the window responsive)
 root.mainloop()
+
+
+
+#Remember that I tried to make the comments seem better and more fun in a way but if it's still bad then tell me:)
+#I want to improve as much as possible.
+#Also I never got the themes to work as well as the ability to save things as I couldn't really understand them.
+
